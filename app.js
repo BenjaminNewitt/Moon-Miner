dea37ce0bfb14378f11836b75385d0316f903965
Benjamin Newitt <benjaminnewitt@gmail.com>
2019-11-15 15:16:53 -0700

    adding game logic

--- app.js
+++ app.js
@@ -1,1 +1,64 @@
-console.log("working");
+// NOTE objects
+
+let clickUpgrades = {
+  drills: {
+    price: 100,
+    quantity: 0,
+    multiplier: 1
+  }
+};
+
+let automaticUpgrades = {
+  rovers: {
+    price: 600,
+    quantity: 0,
+    multiplier: 20
+  }
+};
+
+// NOTE variables
+let cheese = 0;
+
+let manualMultiplier = 1;
+
+// for automatic collection
+let collectionInterval = 0;
+
+// draw variables
+let cheeseElem = document.querySelector("#cheese-count");
+let drillElem = document.querySelector("#drill-count");
+let roverElem = document.querySelector("#rover-count");
+
+// NOTE functions
+
+// update user screen
+function draw() {
+  cheeseElem.innerText = cheese;
+  drillElem.innerText = clickUpgrades.drills.quantity;
+}
+
+// click event
+function mine() {
+  cheese += manualMultiplier;
+  draw();
+}
+
+// automatic collection
+
+function collectAutoUpgrades() {}
+
+function startInterval() {
+  collectionInterval = setInterval(collectAutoUpgrades, 3000);
+}
+
+// NOTE purchase methods
+
+function buyDrill() {
+  if (cheese >= clickUpgrades.drills.price) {
+    clickUpgrades.drills.quantity++;
+    cheese -= clickUpgrades.drills.price;
+    manualMultiplier += clickUpgrades.drills.multiplier;
+    console.log("puchased");
+    draw();
+  }
+}
