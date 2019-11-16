dc21ec38d4fb5ab6ff2a9783ae7aae7a3d617c6a
Benjamin Newitt <benjaminnewitt@gmail.com>
2019-11-16 10:29:22 -0700

    adds upgrades

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 // NOTE objects
 
 let clickUpgrades = {
-  drills: {
+  pickaxes: {
     price: 100,
     quantity: 0,
     multiplier: 1
+  },
+
+  drills: {
+    price: 250,
+    quantity: 0,
+    multiplier: 3
   }
 };
 
 let automaticUpgrades = {
-  rovers: {
+  carts: {
     price: 600,
     quantity: 0,
-    multiplier: 20
+    multiplier: 25
+  },
+
+  rovers: {
+    price: 1000,
+    quantity: 0,
+    multiplier: 50
   }
 };
 
@@ -25,10 +37,18 @@ let automaticMultiplier = 0;
 
 // draw variables
 let cheeseElem = document.querySelector("#cheese-count");
+let pickaxeElem = document.querySelector("#pickaxe-count");
 let drillElem = document.querySelector("#drill-count");
+let cartElem = document.querySelector("#cart-count");
 let roverElem = document.querySelector("#rover-count");
 let manualElem = document.querySelector("#manual-multi-count");
 let automaticElem = document.querySelector("#automatic-multi-count");
+
+let cheeseBtn = document.querySelector("#cheese-button");
+let pickaxeBtn = document.querySelector("#pickaxe-button");
+let drillBtn = document.querySelector("#drill-button");
+let cartBtn = document.querySelector("#cart-button");
+let roverBtn = document.querySelector("#rover-button");
 
 // NOTE functions
 
@@ -39,11 +59,31 @@ function draw() {
   manualElem.innerText = manualMultiplier;
   automaticElem.innerText = automaticMultiplier;
   // upgrade count
+  pickaxeElem.innerText = clickUpgrades.pickaxes.quantity;
   drillElem.innerText = clickUpgrades.drills.quantity;
+  cartElem.innerText = automaticUpgrades.carts.quantity;
   roverElem.innerText = automaticUpgrades.rovers.quantity;
   // upgrade buttons
-  let drillBtn = document.querySelector("#drill-button");
-  let roverBtn = document.querySelector("#rover-button");
+  if (cheese < clickUpgrades.pickaxes.price) {
+    pickaxeBtn.setAttribute("disabled", "true");
+  } else {
+    pickaxeBtn.removeAttribute("disabled");
+  }
+  if (cheese < clickUpgrades.drills.price) {
+    drillBtn.setAttribute("disabled", "true");
+  } else {
+    drillBtn.removeAttribute("disabled");
+  }
+  if (cheese < automaticUpgrades.carts.price) {
+    cartBtn.setAttribute("disabled", "true");
+  } else {
+    cartBtn.removeAttribute("disabled");
+  }
+  if (cheese < automaticUpgrades.rovers.price) {
+    roverBtn.setAttribute("disabled", "true");
+  } else {
+    roverBtn.removeAttribute("disabled");
+  }
 }
 
 // click event
@@ -57,7 +97,6 @@ function mine() {
 function collectAutoUpgrades() {
   if (automaticMultiplier > 0) {
     cheese += automaticMultiplier;
-    console.log(automaticMultiplier);
     draw();
   }
   return;
@@ -70,23 +109,51 @@ function startInterval() {
 // NOTE purchase methods
 
 // manual upgrades
+function buyPickaxe() {
+  if (cheese >= clickUpgrades.pickaxes.price) {
+    clickUpgrades.pickaxes.quantity++;
+    cheese -= clickUpgrades.pickaxes.price;
+    manualMultiplier += clickUpgrades.pickaxes.multiplier;
+    clickUpgrades.pickaxes.price = Math.floor(
+      (clickUpgrades.pickaxes.price *= 1.05)
+    );
+    draw();
+  }
+}
+
 function buyDrill() {
   if (cheese >= clickUpgrades.drills.price) {
     clickUpgrades.drills.quantity++;
     cheese -= clickUpgrades.drills.price;
     manualMultiplier += clickUpgrades.drills.multiplier;
-    console.log("puchased");
+    clickUpgrades.drills.price = Math.floor(
+      (clickUpgrades.drills.price *= 1.05)
+    );
     draw();
   }
 }
 
 // automatic upgrades
+function buyCart() {
+  if (cheese >= automaticUpgrades.carts.price) {
+    automaticUpgrades.carts.quantity++;
+    cheese -= automaticUpgrades.carts.price;
+    automaticMultiplier += automaticUpgrades.carts.multiplier;
+    automaticUpgrades.carts.price = Math.floor(
+      (automaticUpgrades.carts.price *= 1.05)
+    );
+    draw();
+  }
+}
+
 function buyRover() {
   if (cheese >= automaticUpgrades.rovers.price) {
     automaticUpgrades.rovers.quantity++;
     cheese -= automaticUpgrades.rovers.price;
     automaticMultiplier += automaticUpgrades.rovers.multiplier;
-    console.log("puchased");
+    automaticUpgrades.rovers.price = Math.floor(
+      (automaticUpgrades.rovers.price *= 1.05)
+    );
     draw();
   }
 }
@@ -94,3 +161,10 @@ function buyRover() {
 // NOTE start automatic interval
 startInterval();
 draw();
+
+// NOTE dev tools
+
+function cheeseIt() {
+  cheese += 9999;
+  draw();
+}
